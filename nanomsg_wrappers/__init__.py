3b948502fc262e255cab63121b5cd682628aaf6e
Rob Galanakis <rob.galanakis@gmail.com>
2014-07-15 13:10:24 -0500

    Replaced importlib with __import__ for 2.6 compatibility. importlib.import_module wasn't needed.

--- nanomsg_wrappers/__init__.py
+++ nanomsg_wrappers/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 from platform import python_implementation
 import pkgutil
-import importlib
+import sys
 import warnings
 
 _choice = None
@@ -11,16 +11,20 @@ def set_wrapper_choice(name):
     global _choice
     _choice = name
 
+def _imp(name):
+    __import__(name)
+    return sys.modules[name]
+
 def load_wrapper():
     if _choice is not None:
-        return importlib.import_module('_nanomsg_' + _choice)
+        return _imp('_nanomsg_' + _choice)
     default = get_default_for_platform()
     try:
-        return importlib.import_module('_nanomsg_' + default)
+        return _imp('_nanomsg_' + default)
     except ImportError:
         warnings.warn(("Could not load the default wrapper for your platform: "
                        "%s, performance may be affected!") % (default,))
-    return importlib.import_module('_nanomsg_ctypes')
+    return _imp('_nanomsg_ctypes')
 
 def get_default_for_platform():
     if python_implementation() == 'CPython':
